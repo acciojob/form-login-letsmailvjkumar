6d5acddc8b55aa54fc4ecf7535602aa78aeb1604
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-21 13:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElementsByName('fname')[0].value;
+  let lastName = document.getElementsByName('lname')[0].value;
+
+  alert(firstName + ' ' + lastName);
 
 }
